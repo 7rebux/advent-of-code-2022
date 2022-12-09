6a151eb05642b8371e6ac47eb7f1f3f6be996fc2
7rebux <58339645+7rebux@users.noreply.github.com>
2022-12-09 18:41:53 +0100

    :calendar: Day 08 - Treetop Tree House (Part 2)

--- src/main/kotlin/de/nosswald/aoc/days/Day08.kt
+++ src/main/kotlin/de/nosswald/aoc/days/Day08.kt
@@ -16,16 +16,25 @@ object Day08 : Day(8, "Treetop Tree House") {
 
     private fun List<Int>.visibleTreeIndices(transposed: Boolean, index: Int): Set<Pair<Int, Int>> {
         return sequenceOf(
-            *this.indices.map { i -> Pair(this.asReversed().subList(i, this.size), this.lastIndex - i) }.toTypedArray(),
-            *this.indices.map { i -> Pair(this.subList(i, this.size), i) }.toTypedArray()
+            *this.indices.map { i -> this.asReversed().subList(i, this.size) to this.lastIndex - i }.toTypedArray(),
+            *this.indices.map { i -> this.subList(i, this.size) to i }.toTypedArray()
         )
-            .map { (list, i) -> Pair(list.isEmpty() || list.drop(1).all { it < list[0] }, i) }
+            .map { (list, i) -> (list.isEmpty() || list.drop(1).all { it < list[0] }) to i }
             .filter { it.first }
-            .map { if (transposed) Pair(index, it.second) else Pair(it.second, index) }
+            .map { if (transposed) index to it.second else it.second to index }
             .toSet()
     }
 
-    private fun scenicScore(x: Int, y: Int): Int = TODO()
+    private fun scenicScore(x: Int, y: Int): Int {
+        val right = (x + 1 until grid.size).map { it to y }
+        val left = (x - 1 downTo 0).map { it to y }
+        val down = (y + 1 until grid[y].size).map { x to it }
+        val up = (y - 1 downTo 0).map { x to it }
+
+        return listOf(right, left, down, up)
+            .map { minOf(it.takeWhile { (a, b) -> grid[b][a] < grid[y][x] }.size + 1, it.size) }
+            .reduce(Int::times)
+    }
 
     private fun List<List<Int>>.transpose(): List<List<Int>> = List(this.first().size) { i -> this.map { it[i] }.toList() }
 }
