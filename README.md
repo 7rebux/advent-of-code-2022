7b645c7cce2f14b636a241aae52e4674de30b9d3
7rebux <58339645+7rebux@users.noreply.github.com>
2022-12-16 03:11:09 +0100

    :calendar: Day 15 - Beacon Exclusion Zone (Part 2)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 🎄 [Advent Of Code 2022](https://adventofcode.com/2022)
 
 ![Current day](https://img.shields.io/badge/Day-15-blue)
-![Stars obtained](https://img.shields.io/badge/Stars%20Obtained%20⭐-29-yellow)
-![Days completed](https://img.shields.io/badge/Days%20Completed-14-red)
+![Stars obtained](https://img.shields.io/badge/Stars%20Obtained%20⭐-30-yellow)
+![Days completed](https://img.shields.io/badge/Days%20Completed-15-red)
 
 ### Daily Solution Index
 
@@ -22,4 +22,4 @@
 | ✔ 12 | Hill Climbing Algorithm |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/de/nosswald/aoc/days/Day12.kt) |
 | ✔ 13 | Distress Signal         |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/de/nosswald/aoc/days/Day13.kt) |
 | ✔ 14 | Regolith Reservoir      |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/de/nosswald/aoc/days/Day14.kt) |
-| ✔ 15 | Beacon Exclusion Zone   |   ⭐    |   ❌    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/de/nosswald/aoc/days/Day15.kt) |
+| ✔ 15 | Beacon Exclusion Zone   |   ⭐    |   ⭐    | [![Code](https://img.shields.io/badge/Code-grey?style=for-the-badge&logo=Kotlin)](src/main/kotlin/de/nosswald/aoc/days/Day15.kt) |
--- src/main/kotlin/de/nosswald/aoc/days/Day15.kt
+++ src/main/kotlin/de/nosswald/aoc/days/Day15.kt
@@ -6,6 +6,7 @@ import kotlin.math.abs
 
 object Day15 : Day(15, "Beacon Exclusion Zone") {
     private const val ROW = 2_000_000
+    private const val MAX = 4_000_000
     private val closestBeaconToSensor = inputList.map { line ->
         val (sX, sY, bX, bY) = Regex("-?\\d+")
             .findAll(line)
@@ -22,20 +23,38 @@ object Day15 : Day(15, "Beacon Exclusion Zone") {
         closestBeaconToSensor.forEach { (sensor, beacon) ->
             val distance = sensor.distanceTo(beacon)
             val offset = distance - abs(sensor.y - ROW)
-            val left = sensor.x - offset
-            val right = sensor.x + offset
 
-            for (x in left..right)
-                invalid.add(x)
-
-            if (beacon.y == ROW)
-                existing.add(beacon.x)
+            if (beacon.y == ROW) existing.add(beacon.x)
+            (sensor.x - offset..sensor.x + offset).forEach(invalid::add)
         }
 
         return invalid.size - existing.size
     }
 
-    override fun partTwo() = TODO()
+    override fun partTwo(): Long {
+        (0..MAX).forEach { y ->
+            var x = 0
+
+            while (x <= MAX) {
+                val point = Point(x, y)
+                var flag = true
+
+                for ((sensor, beacon) in closestBeaconToSensor) {
+                    if (sensor.distanceTo(beacon) >= sensor.distanceTo(point)) {
+                        flag = false
+                        x = sensor.x + sensor.distanceTo(beacon) - abs(sensor.y - y)
+                        break
+                    }
+                }
+
+                if (flag)
+                    return 1L * point.x * MAX + point.y
+
+                x++
+            }
+        }
+        return -1L
+    }
 
     private fun Point.distanceTo(other: Point): Int {
         return abs(this.x - other.x) + abs(this.y - other.y)
